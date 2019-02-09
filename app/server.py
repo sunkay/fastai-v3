5ec02ba0faa9c6aa6cb735eb8d2c9a685e35657b
sunkay <sunkay11@gmail.com>
2019-02-09 12:25:26 -0800

    check io

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/open?id=11ukQfPm5-_syruJUnez-kxu4vyhpw0d9'
+export_file_name = 'asian-faces.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['japanese', 'chinese', 'korean']
 path = Path(__file__).parent
 
 app = Starlette()
